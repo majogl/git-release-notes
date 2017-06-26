27835638c69125ee0ba5a27851fb76d60e2997d4
Marian Glatzner <glatzner@gmail.com>
2017-06-27 00:09:43 +0200

    additions to python version
    Signed-off-by: Marian Glatzner <glatzner@gmail.com>

--- python/rel_notes.py
+++ python/rel_notes.py
@@ -1,9 +1,14 @@
+#!/usr/bin/python2
 import sys
 import os
 import subprocess
 import json
+from datetime import *
 
-#a = datetime.strptime("Mon Jan 2 14:29:21 2017 +0200", '%a %b %d %H:%M:%S %Y %z')
+#a = datetime.strptime(, '%a %b %d %H:%M:%S %Y %z')
+
+months = {'Jan':1,'Feb':2,'Mar':3,'Apr':4,'May':5,'Jun':6,
+		  'Jul':7,'Aug':8,'Sep':9,'Oct':10,'Nov':11,'Dec':12}
 
 class GetLog:
 	class Commit:
@@ -11,7 +16,8 @@ class GetLog:
 			self.raw = data
 			for key in data:
 				if data[key] != '':
-					command = 'self.{} = "{}"'.format(key,data[key].replace('"','*'))
+					d,k = data[key], key
+					command = 'self.{} = "{}"'.format(k,d.replace('"','*'))
 					exec(command)
 
 		def __str__(self):
@@ -23,7 +29,7 @@ class GetLog:
 	def __init__(self):
 		_COMMAND = ['git','log','--decorate']
 		self.raw = subprocess.check_output(_COMMAND)
-		self.raw = self.raw.decode('utf-8')
+		self.raw = self.raw
 		self.commit_list = []
 		self.process_log()
 
@@ -44,7 +50,15 @@ class GetLog:
 				new_commit['id'] = line.split(' ')[1]
 				if '(' in line:
 					new_commit['tag'] = '(' + line.split('(')[1]
-			elif line.split(' ')[0][:-1] in ['Merge','Author','Date','Change-Id','Signed-off-by']:
+			elif line.split(' ')[0][:-1] == 'Date':
+				temp = line.split(' ')
+				key,data = temp[0][:-1],' '.join(temp[1:])
+				data = (data.strip()).split(' ')
+				#H:M:S D-M-Y
+				data = '{} {}-{}-{}'.format(data[3],data[2],months[data[1]],data[4])
+				print(data)
+				new_commit[key] = data.strip()
+			elif line.split(' ')[0][:-1] in ['Merge','Author','Change-Id','Signed-off-by']:
 				temp = line.split(' ')
 				key,data = temp[0][:-1],' '.join(temp[1:])
 				key = key.replace('-','_')
@@ -53,16 +67,20 @@ class GetLog:
 				else:
 					new_commit[key.strip()] += '||' + data.strip()
 			else:
-				commit_text += str(line)
+				commit_text += line
 	def get_raw_output(self):
 		return self.raw
 
 	def dump(self):
 		with open('result.json','w') as f:
+			res = dict()
 			for commit in self.commit_list:
-				f.write(json.dumps(commit.raw, indent=2, sort_keys=True))
+				res[commit.id] = commit.raw
+			f.write(json.dumps(res, indent=2, sort_keys=True) + '\n')
+			
 
 com = GetLog()
 print(com.commit_list[0])
-com.dump()
+print(com.commit_list[0])
+#com.dump()
 #print(com.get_raw_output())
